078c5d6ded2613287ef139b431ad4dea9c4b67f3
Greg Jopa <gj870q@yp.com>
2015-04-22 16:34:40 -0700

    Configure bootstrap, moment, and highcharts

--- Brocfile.js
+++ Brocfile.js
@@ -2,7 +2,20 @@
 
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
-var app = new EmberApp();
+var app = new EmberApp({
+  lessOptions: {
+    paths: [
+      'bower_components/bootstrap/less'
+    ]
+  },
+  emberHighCharts: {
+    includeHighCharts: true,
+    includeHighStock: false,
+    includeHighMaps: false
+  }
+});
+
+app.import('bower_components/moment/moment.js');
 
 // Use `app.import` to add additional libraries to the generated
 // output files.
